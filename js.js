3510d75ba0a1372d86de30d63a287099d48715a5
bps <>
2026-05-07 00:28:33 -0400

    added more goodies

--- js.js
+++ js.js
@@ -1,6 +1,19 @@
 const project_message = document.querySelector(".project-message");
+const counter_elem = document.querySelector(".counter");
+const title = document.querySelector("h1");
 
-window.addEventListener('mousemove', (e) => {
+window.addEventListener('mousemove', e => {
     project_message.style.left = e.pageX + 'px';
     project_message.style.top = e.pageY + 'px';
+});
+
+let counter = 0;
+window.addEventListener('click', e => {
+    ++counter;
+    counter_elem.textContent = counter;
+    if(counter >= 50 && counter % 2 == 0){
+        title.textContent = "why are you doing this?";
+    }else{
+        title.textContent = "boredprogrammingstudent's website";
+    }
 });
